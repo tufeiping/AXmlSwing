f6f5ee4945dadaff789aaa2d244a7501f3b7ed30
dipoo <120826531@qq.com>
2015-02-10 17:10:07 +0800

    more

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -17,7 +17,7 @@ public class Demo {
 		}
 		long s = System.currentTimeMillis();
 		//加载布局文件
-		GuiXmlLoader.load(Demo.class.getResource("/").getPath() + "/demo2.xml");
+		GuiXmlLoader.load(Demo.class.getResource("/").getPath() + "/demo.xml");
 		System.out.println("渲染时间："+ (System.currentTimeMillis() - s));
 	}
 }
